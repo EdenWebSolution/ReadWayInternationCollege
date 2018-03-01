02d2a57ff1ee45c6632e1b9a99696692517abba1
Thanzeel <tnlthanzeel@gmail.com>
2018-03-01 12:39:16 +0530

    styles moved to bundles and footer added to _layout.cshtml

--- ReadWayInternationCollege/App_Start/BundleConfig.cs
+++ ReadWayInternationCollege/App_Start/BundleConfig.cs
@@ -8,8 +8,33 @@ namespace ReadWayInternationCollege
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+            var googleFntsCDN = "https://fonts.googleapis.com/css?family=Montserrat:600,700%7COpen+Sans:400,600";
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Assets/js/vendor/jquery-1.12.4.min.js",
+                        "~/Assets/js/assets/owl.carousel.min.js",
+                        "~/Assets/js/assets/revolution/jquery.themepunch.revolution.min.js",
+                        "~/Assets/js/assets/revolution/jquery.themepunch.tools.min.js",
+                        "~/Assets/js/assets/jquery.magnific-popup.min.js",
+                        "~/Assets/js/assets/jquery.sticky.js",
+                        "~/Assets/js/assets/jquery.counterup.min.js",
+                        "~/Assets/js/assets/waypoints.min.js",
+                        "~/Assets/js/assets/slick.min.js",
+                        "~/Assets/js/assets/jquery.meanmenu.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.actions.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.carousel.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.kenburn.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.layeranimation.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.migration.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.navigation.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.parallax.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.slideanims.min.js",
+                        "~/Assets/js/assets/revolution/extensions/revolution.extension.video.min.js",
+                        "~/Assets/js/assets/revolution/revolution.js",
+                        "~/Assets/js/custom.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +45,22 @@ namespace ReadWayInternationCollege
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Assets/js/assets/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle("~/Content/css", googleFntsCDN).Include(
+                      "~/Assets/css/assets/bootstrap.min.css",
+                      "~/Assets/css/assets/font-awesome.min.css",
+                      "~/Assets/css/assets/magnific-popup.css",
+                      "~/Assets/css/assets/revolution/layers.css",
+                      "~/Assets/css/assets/revolution/navigation.css",
+                      "~/Assets/css/assets/revolution/settings.css",
+                      
+                      "~/Assets/css/assets/slick.css",
+                      "~/Assets/css/assets/slick-theme.css",
+                      "~/Assets/css/assets/meanmenu.css",
+                      "~/Assets/css/style.css",
+                      "~/Assets/css/responsive.css",
                       "~/Content/site.css"));
         }
     }
