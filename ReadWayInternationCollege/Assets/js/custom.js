7279ba1aaa12b36dc8cce48a4adbeb5d6d7bf9e4
Thanzeel <tnlthanzeel@gmail.com>
2018-04-13 15:14:58 +0530

    unused scripts removed

--- ReadWayInternationCollege/Assets/js/custom.js
+++ ReadWayInternationCollege/Assets/js/custom.js
@@ -18,33 +18,6 @@ $(document).ready(function () {
     })
     $('body').scrollspy({ target: ".edu-navbar'" })
 
-    // Contact Form
-
-    $('#contactform').submit(function () {
-        var action = $(this).attr('action');
-        $("#message").slideUp(750, function () {
-            $('#message').hide();
-            $('#submit')
-                .after('')
-                .attr('disabled', 'disabled');
-            $.post(action, {
-                name: $('#name').val(),
-                email: $('#email').val(),
-                subject: $('#subject').val(),
-                comments: $('#comments').val()
-            },
-                function (data) {
-                    document.getElementById('message').innerHTML = data;
-                    $('#message').slideDown('slow');
-                    $('#contactform img.loader').fadeOut('slow', function () { $(this).remove() });
-                    $('#submit').removeAttr('disabled');
-                    if (data.match('success') != null) $('#contactform').slideUp('slow');
-                }
-            );
-        });
-        return false;
-    });
-
     //Video Popup	
     $('.video-iframe').magnificPopup({
         type: 'iframe',
