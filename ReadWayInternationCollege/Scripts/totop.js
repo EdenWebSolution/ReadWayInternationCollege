af5efba8c0c3e57797fbd9b053f10435cf3c51f8
Thanzeel <tnlthanzeel@gmail.com>
2018-04-20 19:53:21 +0530

    removed comments

--- ReadWayInternationCollege/Scripts/totop.js
+++ ReadWayInternationCollege/Scripts/totop.js
@@ -1,6 +1,6 @@
-(function($) {
+(function ($) {
     var methods = {
-        to_top: function() {
+        to_top: function () {
             $('html, body').animate({
                 scrollTop: 0
             }, 1000);
@@ -8,20 +8,20 @@
         }
     };
 
-    $.fn.tottTop = function(options) {
+    $.fn.tottTop = function (options) {
 
         var settings = $.extend({
-            scrollTop: 800, // Высота с которой будет показываться кнопка
-            duration: 1000, // Длительность анимации скролла
-            scrtollTopBtnDuration: 400 // Длительность анимации отображения кнопки
+            scrollTop: 800,
+            duration: 1000,
+            scrtollTopBtnDuration: 400
         }, options);
 
-        return this.each(function() {
+        return this.each(function () {
             var $this = $(this);
 
             var win = $(window);
 
-            win.scroll(function() {
+            win.scroll(function () {
                 if (win.scrollTop() > settings.scrollTop) {
                     $this.fadeIn(settings.scrtollTopBtnDuration);
                 } else {
